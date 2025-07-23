0d3a4f54e0bcc6691e8e016c8fc93767799f3171
Hai Nam <hainam000090@gmail.com>
2025-07-23 21:47:29 +0700

    add code p2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Harinem 642004!!'
+    return 'Hello from Harinem 642004!!!!!!'
 
 # if __name__=="__main__":
 #     app.run(debug=True)
